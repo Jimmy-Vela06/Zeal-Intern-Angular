738567b1c586fafbaa24781c3d113fc63d26f043
Jimmy Vela <jimmyvela@Zeals-MBP-4.home>
2024-06-13 02:17:07 -0500

    basic grocery

--- intern-project/src/app/grocery-list/grocery-list.component.ts
+++ intern-project/src/app/grocery-list/grocery-list.component.ts
@@ -1,12 +1,55 @@
+// import { Component } from '@angular/core';
+
+// @Component({
+//   selector: 'app-grocery-list',
+//   standalone: true,
+//   imports: [CommonModule],
+//   templateUrl: './grocery-list.component.html',
+//   styleUrl: './grocery-list.component.css'
+// })
+// export class GroceryListComponent {
+
+// }
+
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms'; //
+
+interface GroceryItem {
+  name: string;
+  quantity: number;
+  checked: boolean;
+}
 
 @Component({
   selector: 'app-grocery-list',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './grocery-list.component.html',
-  styleUrl: './grocery-list.component.css'
+  styleUrl: './grocery-list.component.css',
 })
 export class GroceryListComponent {
+  public newItemName: string = '';
+  public newItemQuantity: number = 1;
+  public groceryList: GroceryItem[] = [];
 
+  addItem() {
+    if (this.newItemName && this.newItemQuantity > 0) {
+      this.groceryList.push({
+        name: this.newItemName,
+        quantity: this.newItemQuantity,
+        checked: false,
+      });
+      this.newItemName = '';
+      this.newItemQuantity = 1;
+    }
+  }
+
+  toggleItemChecked(index: number) {
+    this.groceryList[index].checked = !this.groceryList[index].checked;
+  }
+
+  removeItem(index: number) {
+    this.groceryList.splice(index, 1);
+  }
 }
